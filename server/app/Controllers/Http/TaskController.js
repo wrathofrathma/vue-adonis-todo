be3622a3d77682072817a0a6006c412ea831d9f9
Rathma <rathma@kyso.dev>
2021-03-10 05:40:04 -0500

    Added task controller, routes, model, schema

---
+++ server/app/Controllers/Http/TaskController.js
@@ -0,0 +1,56 @@
+'use strict'
+
+const AuthService = use("App/Services/AuthorizationService")
+const Project = use("App/Models/Project")
+const Task = use("App/Models/Task")
+
+class TaskController {
+    async create({request, params, auth}) {
+        const user = await auth.getUser();
+        const { id } = params;
+        const project = await Project.find(id);
+        AuthService.verifyPermission(project, user);
+
+        const { description } = request.all();
+        const task = new Task();
+        task.fill({
+            description
+        });
+        await project.tasks().save(task);
+        return task;
+    }
+
+    async index({auth, params}) {
+        const user = await auth.getUser();
+        const { id } = params;
+        const project = await Project.find(id);
+        AuthService.verifyPermission(project, user);
+        return await project.tasks().fetch();
+    }
+
+    async destroy({auth, params}) {
+        const user = await auth.getUser();
+        //ID here refers to ask ID, in index & create it refers to
+        const { id } = params;
+        const task = await Task.find(id);
+        const project = await Project.find(task.project_id);
+        AuthService.verifyPermission(project, user);
+
+        await task.delete();
+        return task;
+    }
+
+    async update({ auth, params, request }) {
+        const user = await auth.getUser();
+        const { id } = params;
+        const task = await Task.find(id);
+        const project = await Project.find(task.project_id);
+        AuthService.verifyPermission(project, user);
+
+        task.merge(request.only('description'));
+        await task.save();
+        return task;
+    }
+}
+
+module.exports = TaskController
--- server/start/routes.js
+++ server/start/routes.js
@@ -31,4 +31,10 @@ Route.group(() => {
   Route.post('projects', "ProjectController.create").middleware('auth')
   Route.delete('projects/:id', "ProjectController.destroy").middleware('auth')
   Route.patch('projects/:id', "ProjectController.update").middleware('auth')
+
+  Route.post("projects/:id/tasks", "TaskController.create").middleware('auth')
+  Route.get("projects/:id/tasks", "TaskController.index").middleware('auth')
+
+  Route.delete("tasks/:id", "TaskController.destroy").middleware("auth")
+  Route.patch("tasks/:id", "TaskController.update").middleware("auth")
 }).prefix("api")
