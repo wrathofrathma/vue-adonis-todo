5569290d14f5c56ca18024ce4222871daa9ffbb9
Rathma <rathma@kyso.dev>
2021-03-12 13:10:55 -0500

    Enabled adonis static file middleware so we can actually serve the project.

--- server/start/routes.js
+++ server/start/routes.js
@@ -16,9 +16,9 @@
 /** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
 const Route = use('Route')
 
-Route.get('/', () => {
-  return { greeting: 'Hello world in JSON' }
-})
+// Route.get('/', () => {
+//   return { greeting: 'Hello world in JSON' }
+// })
 
 // Groups routes together by a prefix
 Route.group(() => {
